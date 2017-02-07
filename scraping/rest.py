cda283260350671fe2b0f5cea195d78b6a9b8e7c
Raghav Maheshwari <imnobody0396@gmail.com>
2017-02-08 00:50:31 +0530

    Update rest.py

--- scraping/rest.py
+++ scraping/rest.py
@@ -2,40 +2,34 @@ import pycurl
 from io import BytesIO
 from urllib.parse import urlencode
 
-base_url = 'http://54.152.49.226:7000'
+base_url = 'http://54.152.49.226:7000/'
 
-def register(username, password, fullname):
-    c = pycurl.Curl()
-    c.setopt(c.URL, base_url + '/auth/register')
-    post_data = {'username' : username,
-                 'password' : password,
-                 'fullname' : fullname}
-    # Form data must be provided already urlencoded.
-    postfields = urlencode(post_data)
-    # Sets request method to POST,
-    # Content-Type header to application/x-www-form-urlencoded
-    # and data to send in request body.
-    c.setopt(c.POSTFIELDS, postfields)
-    c.perform()
-    # HTTP response code, e.g. 200.
-    print('Status: %d' % c.getinfo(c.RESPONSE_CODE))
-    c.close()
-
-
-def login(username, password):
+def post(url, postfields):
     buffer = BytesIO()
     c = pycurl.Curl()
-    c.setopt(c.URL, base_url + '/auth/login')
-    post_data = {'username' : username,
-                 'password' : password}
-    postfields = urlencode(post_data)
+    c.setopt(c.URL, base_url + url)
     c.setopt(c.POSTFIELDS, postfields)
     c.setopt(c.WRITEDATA, buffer)
     c.perform()
     print('Status: %d' % c.getinfo(c.RESPONSE_CODE))
     c.close()
+    return buffer
+    
 
-    body = buffer.getvalue()
+def register(username, password, fullname):
+    post_data = {'username' : username,
+                 'password' : password,
+                 'fullname' : fullname}
+    postfields = urlencode(post_data)
+
+    post('auth/register', postfields)
+
+
+def login(username, password):
+    post_data = {'username' : username,
+                 'password' : password}
+    postfields = urlencode(post_data)
+    body = post('auth/login', postfields).getvalue()
     print(body.decode('iso-8859-1'))
 
 #login('technovendors','helloworld')
