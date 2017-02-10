0ece28f885041bf4ab8f96a82c8aa2a7632ce676
Nitin Goyal <nitingoyal.dev@gmail.com>
2017-02-11 00:24:58 +0530

    Update mailgun.py

--- scraping/mailgun.py
+++ scraping/mailgun.py
@@ -1,3 +1,4 @@
+from socketIO_client import SocketIO, BaseNamespace
 import requests
 
 key = 'key-d47971b31e4f183660b42cb709f788e4'
@@ -16,4 +17,24 @@ def sendmail(subject, message, recipient):
     print('Body:   {0}'.format(request.text))
 
 
-sendmail('Mail Function', 'Mail sent using Sendmail.', 'raghavpiet108@poornima.org')
+#sendmail('Mail Function', 'Mail sent using Sendmail.', 'raghavpiet108@poornima.org')
+#sendmail('Mail Function', 'Mail sent using Sendmail.', 'nitin.nitingoyal.goyal@gmail.com')
+
+class Namespace(BaseNamespace):
+	def on_connect(self):
+		print('[Connected]')
+
+	def on_reconnect(self):
+		print('[Reconnected]')
+
+	def on_disconnect(self):
+		print('[Disconnected]')
+
+def on_news_response(*args):
+	sendmail('Mail Function', 'Mail sent using Sendmail.' + args, 'raghavpiet108@poornima.org')
+	print('on_news_response', args)
+
+socketIO = SocketIO('54.152.49.226', 7000, Namespace)
+socketIO.on('news', on_news_response)
+#socketIO.emit('news', {'xxx':'yyy'}, on_news_response)
+socketIO.wait(seconds=100)
